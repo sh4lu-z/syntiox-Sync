35cf7f8dc7a4b76b7a19d627f6c310b3aa74f62f
Gimhan <shalukagimhan13@gmail.com>
2026-03-08 22:14:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const CONFIG = {
-    // Socket Server (QR එකට ඕන එක - ඔයාගේ Koyeb එක)
-    SOCKET_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app",
+ 
+    BACKEND_URL: process.env.BACKEND_URL || "",
     
-    // API URL (මේක හිස්ව තියන්න, ඉබේම Vercel ලින්ක් එක ගන්නවා)
+  
     API_BASE: window.location.origin
 };
+
+export default CONFIG;
