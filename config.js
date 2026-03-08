68c3a3aa48b4f206642dfb18d34b014590368b4b
Gimhan <shalukagimhan13@gmail.com>
2026-03-08 22:56:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 const CONFIG = {
- 
-    BACKEND_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app"
+    BACKEND_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app", // මෙතන කොමාව අනිවාර්යයි
     API_BASE: window.location.origin
 };
-
-export default CONFIG;
