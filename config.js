40f5a29c6d9e10e7625ed4c657d9ae1a35a0bb79
Gimhan <shalukagimhan13@gmail.com>
2026-03-08 19:21:41 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const CONFIG = {
+    // ඔයාගේ KOYEB URL එක මෙතනට දාන්න
+    BACKEND_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app",
+    
+    // MongoDB සම්බන්ධ Backend API එක (කෙලින්ම DB එකට Frontend එකෙන් කනෙක්ට් වෙන්න බැහැ ආරක්ෂාව මදි නිසා)
+    API_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app/api/v1",
+    
+    // වෙනත් ඕනෑම Secret Key එකක්
+    APP_VERSION: "1.0.0"
+};
