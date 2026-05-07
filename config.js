bd4fe8952e8d633821c05df680d0e2f8730cf7c7
Shaluka Gimhan <shalukagimhan13@gmail.com>
2026-05-07 13:50:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    BACKEND_URL: "https://uptight-ketti-chamimusic-3cce977f.koyeb.app", 
+    BACKEND_URL: "https://meaningful-eliza-chamimusic-afc92c75.koyeb.app", 
     API_BASE: window.location.origin
 
 };
